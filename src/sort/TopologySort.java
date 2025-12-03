2db645b6f618694ac32e294a220970e5757697bd
subin930 <bread0930@g.skku.edu>
2025-12-03 14:38:46 +0900

    위상 정렬 주석 추가

--- src/sort/TopologySort.java
+++ src/sort/TopologySort.java
@@ -34,6 +34,15 @@ public class TopologySort {
         graph.get(u).add(v);
     }
 
+    /*
+        1. 방문 여부 배열 visited[] 준비
+        2. 모든 노드에 대해 방문하지 않았다면 DFS 실행
+        3. DFS(node)
+            1. node 방문 표시
+            2. node의 모든 자식(node → next) 재귀 DFS
+            3. node를 스택에 push
+        4. DFS 종료 시 스택을 뒤집거나 pop 순서대로 출력
+     */
     static void dfsTopo(int node, boolean[] visited, Stack<Integer> stack, List<List<Integer>> graph) {
         visited[node] = true;
         for(int next : graph.get(node)) {
@@ -57,6 +66,15 @@ public class TopologySort {
         System.out.println();
     }
 
+    /*
+        1. 각 노드의 진입차수 계산
+        2. 진입 차수가 0인 노드를 큐에 넣음
+        3. 큐가 빌 때까지 반복
+            1. 큐에서 노드를 꺼내 배열에 추가
+            2. 해당 노드의 자식 노드들의 진입 차수를 -1
+            3. 새로 진입차수가 0된 노드들을 큐에 추가
+        4. 모든 노드를 처리 완료했으면 Topo 순서 완성
+     */
     static void topoSortBFS(int N, List<List<Integer>> graph) {
         int[] indegree = new int[N];
 
